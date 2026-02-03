a8c9f17943b9d10af105f1a5ac99f731e34cdcb5
Brennan <148926321+Brenn26@users.noreply.github.com>
2026-02-03 18:57:16 -0500

    fixed includes

--- src/masternode/masternodeman.cpp
+++ src/masternode/masternodeman.cpp
@@ -3,6 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "masternodeman.h"
+#include "clientversion.h"
 #include "fs.h"
 #include "streams.h"
 #include "util.h"
