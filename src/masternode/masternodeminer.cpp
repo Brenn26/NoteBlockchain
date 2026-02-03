071215a7cafec395eef1afedf1923594c0d471bf
Brennan <148926321+Brenn26@users.noreply.github.com>
2026-02-03 15:39:53 -0500

    PoS reward calc fix

--- src/masternode/masternodeminer.cpp
+++ src/masternode/masternodeminer.cpp
@@ -179,18 +179,16 @@ bool CMasternodeMiner::CreateCoinStake(const CChainParams& chainparams,
                 txNewMut.vin.resize(1);
                 txNewMut.vin[0].prevout = prevout;
 
+                // Calculate stake reward
+                CAmount nReward = GetBlockSubsidy(pindexPrev->nHeight + 1, params);
+
                 // Output 0: empty (kernel marker)
-                txNewMut.vout.resize(3);
+                txNewMut.vout.resize(2);
                 txNewMut.vout[0].SetEmpty();
 
-                // Output 1: return collateral to same address
-                txNewMut.vout[1].nValue = params.nMasternodeCollateral;
+                // Output 1: return collateral + reward to same address
+                txNewMut.vout[1].nValue = params.nMasternodeCollateral + nReward;
                 txNewMut.vout[1].scriptPubKey = wtx->tx->vout[coin.i].scriptPubKey;
-
-                // Output 2: stake reward
-                CAmount nReward = GetBlockSubsidy(pindexPrev->nHeight + 1, params);
-                txNewMut.vout[2].nValue = nReward;
-                txNewMut.vout[2].scriptPubKey = wtx->tx->vout[coin.i].scriptPubKey;
 
                 // Sign the transaction
                 const CKeyStore& keystore = *pwallet;
